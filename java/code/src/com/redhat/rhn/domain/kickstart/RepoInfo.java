712c212133a7f159e50991d9c0f1378085406c01
Matei Albu <malbu@suse.de>
2020-09-03 14:40:07 +0200

    Fix repo url of AppStream in generated RHEL/Centos 8 kickstart file (bsc#1175739)

--- java/code/src/com/redhat/rhn/domain/kickstart/RepoInfo.java
+++ java/code/src/com/redhat/rhn/domain/kickstart/RepoInfo.java
@@ -119,12 +119,8 @@ public class RepoInfo {
     public String getFormattedCommand(KickstartData data) {
         KickstartUrlHelper helper = new KickstartUrlHelper(data);
         String repourl = helper.getRepoUrl(this);
-        if (name.equals("AppStream")) {
-            /* RHEL 8 AppStream */
-            repourl = helper.getKickstartMediaUrl().replace("baseos", "appstream");
-        }
-        return String.format("repo --name=%s --baseurl=%s", name,
-                                    repourl);
+        repourl = String.format("%s%s%s", repourl, repourl.endsWith("/") ? "" : "/", name);
+        return String.format("repo --name=%s --baseurl=%s", name, repourl);
     }
 
     /**
